39abc88bac3e61880bdf57a960844c1b11dd23d6
Anna Evdokimova <evdockimova.annie@yandex.ru>
2024-01-22 19:56:55 +0300

    Fixed errors

--- searchInArray/searchInArray.cpp
+++ searchInArray/searchInArray.cpp
@@ -4,62 +4,37 @@ int getCount(int* arr, int size, int target)
 {
     int left = 0;
     int right = size - 1;
-    int index;
+    int index = 0;
 
-    while (left < right)
+    if (arr[right] <= target)
     {
-        int mid;
-        mid = (left + right) / 2;
-        if (arr[size - 1] < target)
-        {
-            index = size;
-            break;
-        }
-
-        if (right - left == 2)
-        {
-            if (arr[left] > target)
-            {
-                index = 0;
-                break;
-            }
-            else if (arr[right] < target)
-            {
-                index = right;
-                break;
-            }
-            index = mid;
-            break;
-        }
-
-        if (right - left == 1)
-        {
-            if (arr[left] > target)
-            {
-                index = 0;
-                break;
-            }
-            else if (arr[right] <= target)
-            {
-                index = right + 1;
-                break;
-            }
-        }
-
-        if (arr[mid] > target)
-        {
-            right = mid + 1;
-        }
-        else if (arr[mid] < target)
-        {
-            left = mid - 1;
-        }
-        else
-        {
-            left = mid;
-        }
+        index = size;
     }
+    else if (arr[left] > target)
+    {
+        index = 0;
+    }
+    else
+    {
+        while (left < right)
+        {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target)
+            {
+                left = mid + 1;
+            }
+            else if (arr[mid] > target)
+            {
+                right = mid;
+            }
+            else if (arr[mid] < target)
+            {
+                left = mid + 1;
+            }
 
+        }
+        index = right;
+    }
     int num = size - index;
     return num;
 }
